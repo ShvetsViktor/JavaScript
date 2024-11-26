9a3732613ba822e24cc03cb62e8a21eadf6ce972
viktor_shvets <shvetsviktor89@gmail.com>
2024-11-26 11:34:55 +0000

    edited script

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@
 
 const arr = [2, 3, 6, 8, 10];
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}  // перебор через классический цикл
 
 for (let value of arr) {
     console.log(value)
