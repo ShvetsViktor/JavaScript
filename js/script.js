09f44d9b41cd7675a79d5f79fed3a21d4247e757
viktor_shvets <shvetsviktor89@gmail.com>
2024-11-27 14:42:23 +0000

    Spread operator ...

--- js/script.js
+++ js/script.js
@@ -1,44 +1,44 @@
 "use strict"
 
-function copy(mainObj) {
-    let copyObj = {};
 
-    let key;
-    for (key in mainObj) {
-        copyObj[key] = mainObj[key]
-    }
-    return copyObj
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[0] = 'z';
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'drupal', 'joomla', 'Squarespace'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
+const num = [2, 5, 7];
 
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+console.log(newAarray);
+
+const q = {
+    one: 1,
+    two: 2
 };
 
-const newNumbers = copy(numbers);
+newObj = {...q};
 
-newNumbers.a = 10;
+q.one = '3';
 
-console.log(newNumbers)
-console.log(numbers)
-
-
-
-console.log(Object.assign(numbers, add));
-
-const add = {
-    d: 23,
-    e: 34
-}
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-console.log(add);
-console.log(clone);
+console.log(newObj);
+console.log(q);
