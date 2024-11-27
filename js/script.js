62d80db3224fd7a483489044f1a4883cd4743d4b
viktor_shvets <shvetsviktor89@gmail.com>
2024-11-27 14:42:23 +0000

    Spread operator ...

--- js/script.js
+++ js/script.js
@@ -1,5 +1,47 @@
 "use strict"
 
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[0] = 'z';
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'drupal', 'joomla', 'Squarespace'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+console.log(newAarray);
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+newObj = {...q};
+
+q.one = '3';
+
+console.log(newObj);
+console.log(q);
 // const arr = [ 'apple', 'mango', 'papaya', 'dorian', 'greypfruit' ];
 
 const arr = [1, 35, 13, 2, 23 ];
