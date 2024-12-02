9bce7ed7e235d434c9938af472fcadbe3c9d6058
viktor_shvets <shvetsviktor89@gmail.com>
2024-12-02 10:52:04 +0000

    working with objects and methods

--- js/script.js
+++ js/script.js
@@ -1,123 +1,74 @@
-// "use strict"
+"use strict"
 
 
-// const arr = [ 'apple', 'mango', 'papaya', 'dorian', 'greypfruit' ];
-
-const arr = [1, 35, 13, 2, 23 ];
-arr.sort(compareNum);
-
-console.log(arr);
-
-function compareNum(a, b) {
-    return a - b;
-}
-
-function copy(mainObj) {
-    let copyObj = {};
-
-    let key;
-    for (key in mainObj) {
-        copyObj[key] = mainObj[key]
-    }
-    return copyObj
-}
-// const newNumbers = copy(numbers);
-
-// newNumbers.a = 10;
-
-// console.log(newNumbers)
-// console.log(numbers)
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-
-};
-
-const add = {
-    d: 23,
-    e: 34
-};
-
-console.log(Object.assign(numbers, add)); // Копируем свойства объекта add в объект numbers
-
-const clone = Object.assign({}, add); // Создаём поверхностную копию объекта add
-
-
-
-clone.d = 20;
-
-console.log(add);
-console.log(clone);
-
-const q = {
-    one: 1,
-    two: 2
-};
-
-const personalPlanPeter = {
-    name: "Peter",
-    age: "29",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%'
-        },
-        exp: '1 month'
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        this.count = +prompt('Сколько фильмов вы уже просмотрели?', '');
+        
+        while (this.count == null || isNaN(this.count) || this.count == '') {
+            this.count = +prompt('Сколько фильмов вы уже просмотрели?', '');
+        }
     },
-};
-
-function showAgeAndLangs(object) {
-    const {age} = object;
-    const {languages} = object.skills;
-    let str = `Мне ${age} и я владею языками: `; 
-    languages.forEach(function(lang) {
-        str += `${lang.toUpperCase()} `;
-    });
-    return str;
-};
-
-console.log(showAgeAndLangs(personalPlanPeter));
-
-const someString = 'This is some strange string';
-
-function reverse(str) {
-    let array = [];
-    let newArray = '';
-
-    if (typeof(str) === 'string') {
-        array = str.split('');
-        array.reverse();
-        newArray = array.join('');
-    } else {
-        newArray += 'Ошибка!';
-    }
-
-    return newArray
-}
-
-console.log(reverse(someString));
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-let allCurr = [...baseCurrencies, ...additionalCurrencies];
-
-
-function availableCurr(arr, missingCurr) {
-        let outPut = arr.length <= 0 ? `Нет доступных валют` : `Доступные валюты:\n`;
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+                  b = prompt('На сколько оцените его?', '');
         
-        for (let i of arr) {
-            if (i !== missingCurr) {
-                outPut += `${i}\n`;
-                }
+            if (a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+                this.movies[a] = b;
+                console.log("done");
+            } else {
+                console.log('error');
+                i--;
             }
-        
-        return outPut;
-}
+        }
+    },
+    detectPersonalLevel: function() {
+        if (this.count <= 10) {
+            console.log('Просмотрено слишком мало фильмов.');
+        } else if (this.count <= 30 && this.count >= 10) {
+            console.log('Вы классический зритель.');
+        } else if (this.count > 30) {
+            console.log('Вы киноман.');
+        } else {
+            console.log('Произошла ошибка.');
+        }
+    },
+    showMyDB: function() {
+        if (this.privat == false) {
+            console.log(this);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i = 1; i <= 3; i++) {
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
+            
+            if (genre === '' || genre === null) {
+                i--;
+            } else {
+                this.genres[i - 1] = genre;
+                this.genres.sort();
+            }
+        }
+        this.genres.forEach((genre, index) => console.log(`Любимый жанр #${index + 1} - это ${genre}`));
+    },
+    toggleVisibleMyDB: function() {
+        if (this.privat) {
+            this.privat = false
+        } else {
+            this.privat = true
+        }
+    }
+};
 
-console.log(availableCurr(allCurr, 'UAH'));
+personalMovieDB.start();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.writeYourGenres();
+personalMovieDB.detectPersonalLevel();
+personalMovieDB.showMyDB();
