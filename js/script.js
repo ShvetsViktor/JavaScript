f1289690b0a4876adff42d794e4cc412128582f5
viktor_shvets <shvetsviktor89@gmail.com>
2024-12-03 18:44:00 +0000

    payment branch created

--- js/script.js
+++ js/script.js
@@ -1,44 +1,12 @@
 "use strict"
 
-function copy(mainObj) {
-    let copyObj = {};
 
-    let key;
-    for (key in mainObj) {
-        copyObj[key] = mainObj[key]
-    }
-    return copyObj
+const arr = [2, 3, 6, 8, 10];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}  // перебор через классический цикл
+
+for (let value of arr) {
+    console.log(value)
 }
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-
-console.log(newNumbers)
-console.log(numbers)
-
-
-
-console.log(Object.assign(numbers, add));
-
-const add = {
-    d: 23,
-    e: 34
-}
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-console.log(add);
-console.log(clone);
