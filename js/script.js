2911e24d1e9580462ef8ae951c3d2d1b0f25e64b
viktor_shvets <shvetsviktor89@gmail.com>
2024-12-04 09:19:59 +0000

    refactoring

--- js/script.js
+++ js/script.js
@@ -328,14 +328,3 @@ console.log(sortStudentsByGroups(students));
 console.log(sortStudentsByGroups(students));
 
 
-// Merged payment wiuth main 
-
-
-const arr = [2, 3, 6, 8, 10];
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}  // перебор через классический цикл
-
-for (let value of arr) {
-    console.log(value)
