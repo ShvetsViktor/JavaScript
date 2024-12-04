1dc64c5d418009e84b048e49f61d2a98eefa9d5d
viktor_shvets <shvetsviktor89@gmail.com>
2024-12-04 09:17:45 +0000

    wiped payment

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 "use strict"
 
 
-const arr = [2, 3, 6, 8, 10];
-
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}  // перебор через классический цикл
-
-for (let value of arr) {
-    console.log(value)
-}
