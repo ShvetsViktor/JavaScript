baa62f1c598625c2e5b0b7857d59dd55515f75b5
viktor_shvets <shvetsviktor89@gmail.com>
2024-12-04 13:41:31 +0000

    Dynamical typisation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,62 @@
 "use strict"
 
 
+// Динамическая типизация и примеры её использования
+// Способы преобразований объектов в другие типы данных.
+
+// Преобразования To String
+
+// #1
+
+console.log(typeof(String(null))); // Избыточен. Не используется.
+console.log(typeof(String(4))); // Избыточен. Не используется.
+
+// #2
+
+console.log(typeof('' + 5)); // Конкотэнация со строкой даёт строку
+
+const num = 5;
+
+console.log("https://facebook.com/catalog/" + num);
+
+// Реальный пример использования конкотенации. Передавать стили в виде строки
+
+const fontSize = 26 + 'px'; //  Формируется строка
+
+
+// Преобразования To Number
+
+// # 1
+
+console.log(typeof(Number('4')));
+
+// # 2
+// Унарный плюс
+
+console.log(typeof(+'5'));
+
+// # 3
+
+console.log(typeof(parseInt("15px", 10)));
+
+// let answ = +prompt("Hello", "");
+
+// Преобразования To Boolean
+
+// # 1
+
+let switcher = null;
+
+// switcher = 1;
+
+console.log(switcher ? "Working ..." : 'Boolean');
+
+// # 2
+
+console.log(typeof(Boolean('4')));
+
+// # 3
+
+console.log(typeof(!!"223")); // Два знака отрицания 
+                              // преобразуют строку в логический 
+                              // тип данных
