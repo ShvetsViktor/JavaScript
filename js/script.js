dc73801eff3ce27cf49837ea23be94323322fb2f
viktor_shvets <shvetsviktor89@gmail.com>
2024-12-05 15:32:10 +0000

    some minor changes

--- js/script.js
+++ js/script.js
@@ -734,3 +734,37 @@ console.log(sortStudentsByGroups(students));
 // console.log(sortStudentsByGroups(students));
 
 
+"use strict"
+
+
+// let number = 5; debugger
+
+// function logNumber() {
+//     console.log(number); debugger
+// }
+
+// number = 6; debugger
+
+// logNumber();
+
+
+
+function createCounter() {
+    let counter = 0;
+
+    const myFunction = function() {debugger
+        counter = counter + 1; debugger
+        return counter;debugger
+    }
+
+    return myFunction;
+}
+debugger
+// Function expression
+
+const increment = createCounter();
+const c1 = increment();debugger
+const c2 = increment();debugger
+const c3 = increment();debugger
+
+console.log(c1, c2, c3);
